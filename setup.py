558310ba33649a85e0d23a4ef11d8c2e2f056346
Przemek <nagat@ip-172-31-7-140.eu-west-2.compute.internal>
2018-08-15 15:41:44 +0000

    S

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="lfdeploy",
-    version="1.2.1",
+    version="1.2.2",
     author="Przemyslaw Winszczyk",
     author_email="n3onix@gmail.com",
     description="Improved tfdeploy module",
